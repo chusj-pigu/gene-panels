1ed9090e9e2a7cf49d398f19ccfd5612d2b24b23
Nicholas <5053806+bwbioinfo@users.noreply.github.com>
2025-02-05 13:04:23 -0500

    Added small script and generated most recent updated panels. Also generated excel file with links to UCSC and ENSEMBL.

---
+++ scripts/quickpanel.R
@@ -0,0 +1,99 @@
+#!/usr/bin/env Rscript
+
+# Load required libraries
+suppressPackageStartupMessages({
+    library(optparse)
+    library(biomaRt)
+    library(tidyverse)
+    library(dplyr)
+})
+# ---- Define options ----
+option_list <- list(
+    make_option(c("-g", "--genes"), type = "character", default = "sj-panel/genes_list.txt",
+                            help = "Path to the gene list file [default: %default]", metavar = "FILE"),
+    make_option(c("-o", "--output"), type = "character", default = "sj-panel/CHUSJ-2025-Adaptive-Panel-LP-Genes.bed",
+                            help = "Path to the output file [default: %default]", metavar = "FILE"),
+    make_option(c("-p", "--padding"), type = "integer", default = 20000,
+                            help = "Padding for start and end positions [default: %default]", metavar = "NUMBER"),
+    make_option(c("-d", "--genome"), type = "character", default = "hg38",
+                            help = "UCSC genome version (e.g., hg38, hg19) [default: %default]", metavar = "STRING"),
+    make_option(c("-l", "--loci"), type = "character", default = "sj-panel/loci.txt",
+                            help = "Path to the file with additional loci [default: %default]", metavar = "FILE"),
+    make_option(c("-r", "--outdir"), type = "character", default = ".",
+                            help = "Output directory [default: %default]", metavar = "DIR")
+)
+
+# ---- Parse options ----
+opt <- parse_args(OptionParser(option_list = option_list))
+
+# ---- Read the gene list ----
+genes <- readLines(opt$genes)
+
+# ---- Connect to Ensembl BioMart ----
+ensembl <- useMart("ensembl", dataset = "hsapiens_gene_ensembl")
+
+# ---- Retrieve genomic loci information ----
+gene_loci <- getBM(
+    attributes = c(
+        "hgnc_symbol",
+        "chromosome_name",
+        "start_position",
+        "end_position",
+        "strand"
+    ),
+    filters = "hgnc_symbol",
+    values = genes,
+    mart = ensembl
+)
+
+# ---- Filter and format the data ----
+gene_loci_canonical <- gene_loci |>
+    filter(chromosome_name %in% c(as.character(1:22), "X", "Y"))
+
+# ---- Add manual loci ----
+read_tsv(opt$loci, col_types = c("cciii")) |>
+    bind_rows(gene_loci_canonical) |>
+    mutate(chromosome_name = factor(chromosome_name, levels = c(as.character(1:22), "X", "Y"))) ->
+    gene_loci_canonical
+
+# ---- Add padding, generate gene IDs, and prepare for BED format ----
+gene_loci_bed <- gene_loci_canonical |>
+    arrange(chromosome_name, start_position) |>
+    mutate(row_number = row_number()) |>
+    mutate(gene_index = str_pad(row_number, 4, pad = "0")) |>
+    mutate(gene_id = paste0(gene_index, "_", "chr", chromosome_name, "_", hgnc_symbol)) |>
+    mutate(strand = if_else(strand == 1, "+", "-")) |>
+    rename(start = start_position, end = end_position, chrom = chromosome_name) |>
+    mutate(start = pmax(0, start - opt$padding), end = end + opt$padding) |>
+    mutate(chrom = paste0("chr", chrom)) |>
+    select(chrom, start, end, gene_id, strand) |>
+    mutate(
+        ucsc_link = paste0(
+            "https://genome.ucsc.edu/cgi-bin/hgTracks?db=",
+            opt$genome,
+            "&position=",
+            chrom,
+            ":",
+            start,
+            "-",
+            end
+        )
+    ) |>
+    mutate(
+        ensembl_link = paste0(
+            "https://www.ensembl.org/Homo_sapiens/Location/View?r=",
+            chrom,
+            ":",
+            start,
+            "-",
+            end
+        )
+    ) 
+
+gene_loci_bed |>
+    writexl::write_xlsx(paste0(opt$outdir,"/",Sys.Date(), "-CHUSJ-Panel-",opt$padding/1000, "kb-",  opt$output, ".xlsx"))
+
+
+gene_loci_bed |>
+    select(chrom, start, end, gene_id) |>
+    write_tsv(paste0(opt$outdir,"/", Sys.Date(), "-CHUSJ-Panel-",opt$padding/1000, "kb-", opt$output, ".bed"), col_names = FALSE)
